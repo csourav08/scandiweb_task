0acde4a7ce8119a64814d33f824f4b3d427d8a35
Sourav Chowdhury <souravchowdhury@Souravs-MacBook-Air.local>
2021-12-09 14:32:40 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import React, { Component } from "react"
 import Welcome from "./components/category.jsx"
 import Topbar from "./components/topbar/topbar"
+import Bodybar from "./components/category/category"
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Welcome />
         <Topbar />
+        <Bodybar />
       </div>
     )
   }
---
+++ src/components/category/category.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react"
+import "./category.css"
+class Bodybar extends Component {
+  render() {
+    return <div className="first">Category Name</div>
+  }
+}
+export default Bodybar
